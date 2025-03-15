055314b4d7dd7ff72c9f3ea0ee044497a35ac72e
Karl Keller <karl@komakode.com>
2025-03-15 12:43:13 -0400

    Initial Commit

---
+++ ScoreKeep/ScoreKeepApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreKeepApp.swift
+//  ScoreKeep
+//
+//  Created by Karl Keller on 3/15/25.
+//
+
+import SwiftUI
+
+@main
+struct ScoreKeepApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ScoreKeepTests/ScoreKeepTests.swift
@@ -0,0 +1,17 @@
+//
+//  ScoreKeepTests.swift
+//  ScoreKeepTests
+//
+//  Created by Karl Keller on 3/15/25.
+//
+
+import Testing
+@testable import ScoreKeep
+
+struct ScoreKeepTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
