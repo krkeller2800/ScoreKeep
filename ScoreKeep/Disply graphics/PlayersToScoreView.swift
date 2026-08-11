3e5b9eba085baf57a360cf5a888b80a1430c8c7c
Karl Keller <karl@komakode.com>
2026-08-11 10:25:02 -0400

    Fix scorecard vertical content inset

--- ScoreKeep/Disply graphics/PlayersToScoreView.swift
+++ ScoreKeep/Disply graphics/PlayersToScoreView.swift
@@ -167,6 +167,7 @@ struct PlayersToScoreView: View {
                                 ScorecardScrollViewObserver(controller: scorecardScrollController)
                             )
                         }
+                        .contentMargins(.top, 0, for: .scrollContent)
                         .coordinateSpace(name: "pitcher_vertical_scroll")
                         .onChange(of: pitcherSectionScrollRequest) { _, request in
                             guard let request else { return }
